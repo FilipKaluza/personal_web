634dc9706b17050416696308aef6bfa5b221494f
FilipKaluza <kaluzafilip33@gmail.com>
2021-01-22 15:44:04 +0100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import border from './assets/images/main_border.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={border} className="App-logo" alt="KALUZA.DEV" /> 
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
